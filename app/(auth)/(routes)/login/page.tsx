670eae997590f807a2332518a721836f1c8b02a0
Khanaru220 <calsharp0202@yahoo.com>
2023-08-01 17:05:00 +0700

    Chapter: Intro + Environment Setup + Folder Setup

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,4 @@
+const LoginPage = () => {
+	return <div>LoginPage (Unprotected)</div>;
+};
+export default LoginPage;
---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,4 @@
+const RootPage = () => {
+	return <div>RootPage (Protected)</div>;
+};
+export default RootPage;
