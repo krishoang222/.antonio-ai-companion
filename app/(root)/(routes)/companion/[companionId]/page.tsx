3446cd37bc58f35559c87a4f51b1db45077f267f
Khanaru220 <calsharp0202@yahoo.com>
2023-08-02 16:37:06 +0700

    Chapter: Companion Creation Form - cloudinary + preview img + form with select, input, img, textarea

---
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -0,0 +1,36 @@
+import { auth, redirectToSignIn, redirectToSignUp } from '@clerk/nextjs';
+
+import prismadb from '@/lib/prismadb';
+import CompanionForm from './components/companion-form';
+
+interface CompanionIdPageProps {
+	params: {
+		companionId: string;
+	};
+}
+
+const CompanionIdPage = async ({ params }: CompanionIdPageProps) => {
+	// TODO: Check subscription
+	const { userId } = auth();
+
+	if (!userId) {
+		return redirectToSignUp();
+	}
+
+	const companion = await prismadb.companion.findUnique({
+		where: {
+			id: params.companionId,
+			// userId,
+		},
+	});
+
+	const categories = await prismadb.category.findMany();
+
+	return (
+		<CompanionForm
+			categories={categories}
+			initialData={companion}
+		></CompanionForm>
+	);
+};
+export default CompanionIdPage;
