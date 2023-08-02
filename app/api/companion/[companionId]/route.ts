c3c91aab7c24929ab8dda10459aa746551487ab5
Khanaru220 <calsharp0202@yahoo.com>
2023-08-03 01:51:06 +0700

    Chapter: Chat Header  - avatar + back button + menu button

--- app/api/companion/[companionId]/route.ts
+++ app/api/companion/[companionId]/route.ts
@@ -1,6 +1,7 @@
-import prismadb from '@/lib/prismadb';
 import { auth, currentUser } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
+
+import prismadb from '@/lib/prismadb';
 
 export async function PATCH(
 	req: Request,
@@ -51,6 +52,31 @@ export async function PATCH(
 		return NextResponse.json(companion);
 	} catch (error) {
 		console.log('[COMPANION_PATCH]', error);
+		return new NextResponse('Internal Error', { status: 500 });
+	}
+}
+
+export async function DELETE(
+	req: Request,
+	{ params }: { params: { companionId: string } }
+) {
+	try {
+		const { userId } = auth();
+
+		if (!userId) {
+			return new NextResponse('Unauthorized', { status: 401 });
+		}
+
+		const companion = await prismadb.companion.delete({
+			where: {
+				userId,
+				id: params.companionId,
+			},
+		});
+
+		return NextResponse.json(companion);
+	} catch (error) {
+		console.log('[COMPANION_DELETE]', error);
 		return new NextResponse('Internal Error', { status: 500 });
 	}
 }
